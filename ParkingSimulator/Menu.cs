b7de1236d0a64398271d55e0fc84a551db8ffbd6
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-05-09 22:50:42 +0300

    Implement "Add car"

--- ParkingSimulator/Menu.cs
+++ ParkingSimulator/Menu.cs
@@ -8,6 +8,7 @@ namespace ParkingSimulator
         {
             do
             {
+                Console.Clear();
                 Console.WriteLine("Select an action and enter its number:\n" +
                                   "1)Add car \n" +
                                   "2)Remove car \n" +
@@ -15,14 +16,42 @@ namespace ParkingSimulator
                                   "4)Show transaction history for the last minute\n" +
                                   "5)Show Parking balance \n"+
                                   "6)Show count of free spots \n" +
-                                  "7)Show Transactions.log");
+                                  "7)Show Transactions.log\n" +
+                                  "8)Show cars");
                 int.TryParse(Console.ReadLine(),out int choise);
                 switch (choise)
                 {
-                   
+                    case 1:
+                        AddCar_Menu();
+                        break;
+                    case 8:
+                        ShowCars_Menu();
+                        break;
+                        
                 }
                 Console.WriteLine("Please, for exit enter ESCAPE and to continue - any other key ");
             }   while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        private static void AddCar_Menu()
+        {
+            Console.WriteLine("Select car type:\n +" +
+                              "1)Passenger\n" +
+                              "2)Truck \n" +
+                              "3)Bus \n" +
+                              "4)Motorcycle");
+            int.TryParse(Console.ReadLine(), out int type);
+            Console.WriteLine("Input balance");
+            int.TryParse(Console.ReadLine(), out int balance);
+            Parking.Instance.AddCar(new Car(balance, (Car.CarType) Enum.Parse(typeof(Car.CarType), (type-1).ToString())));
+        }
+
+        private static void ShowCars_Menu()
+        {
+            foreach (var car in Parking.Instance.Cars)
+            {
+                Console.WriteLine(car.TypeOfTransport.ToString());
+            }
         }
     }
 }
